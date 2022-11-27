99acdd1d72660cd474aa06522ddffc3c07025066
SimoneBersani <sbersani.info@gmail.com>
2022-11-27 20:00:31 +0100

    [EDIT] Print fix

--- esp8266/include/LedMapping.h
+++ esp8266/include/LedMapping.h
@@ -4,35 +4,36 @@
 
 #include "LedWord.h"
 
-const LedWord PREFIX_IT = LedWord(0, 0, 2); // ES
-const LedWord PREFIX_IS = LedWord(3, 0, 3); // IST
+const LedWord PREFIX_IT = LedWord(0, 5, 1); // ES
+const LedWord PREFIX_IS = LedWord(0, 5, 1); // IST
 
-const LedWord INFIX_BEFORE = LedWord(0, 3, 3); // VOR
-const LedWord INFIX_AFTER = LedWord(7, 3, 4); // NACH
+const LedWord INFIX_BEFORE = LedWord(0, 5, 1); // VOR
+const LedWord INFIX_AFTER = LedWord(0, 5, 1);  // NACH
 
-const LedWord SUFFIX_OCLOCK = LedWord(8, 9, 3); // UHR
+const LedWord SUFFIX_OCLOCK = LedWord(0, 5, 1); // UHR
 
-const LedWord MINUTE_FIVE = LedWord(7, 0, 4); // FÜNF
-const LedWord MINUTE_TEN = LedWord(0, 1, 4); // ZEHN
-const LedWord MINUTE_QUARTER = LedWord(4, 2, 7); // VIERTEL
-const LedWord MINUTE_TWENTY = LedWord(4, 1, 7); // ZWANZIG
-const LedWord MINUTE_HALF = LedWord(0, 4, 4); // HALB
-const LedWord MINUTE_THREEQUARTER = LedWord(0, 2, 11); // THREEVIERTEL
+const LedWord MINUTE_FIVE = LedWord(0, 5, 1);         // FÜNF
+const LedWord MINUTE_TEN = LedWord(0, 5, 1);          // ZEHN
+const LedWord MINUTE_QUARTER = LedWord(0, 5, 1);      // VIERTEL
+const LedWord MINUTE_TWENTY = LedWord(0, 5, 1);       // ZWANZIG
+const LedWord MINUTE_HALF = LedWord(0, 5, 1);         // HALB
+const LedWord MINUTE_THREEQUARTER = LedWord(0, 5, 1); // THREEVIERTEL
 
-const LedWord HOURS[] = {   LedWord(0, 5, 4), // EINS
-                            LedWord(7, 5, 4), // ZWEI
-                            LedWord(0, 6, 4), // DREI
-                            LedWord(7, 6, 4), // VIER
-                            LedWord(7, 4, 4), // FÜNF
-                            LedWord(0, 7, 5), // SECHS
-                            LedWord(0, 8, 6), // SIEBEN
-                            LedWord(7, 7, 4), // ACHT
-                            LedWord(3, 9, 4), // NEUN
-                            LedWord(0, 9, 4), // ZEHN
-                            LedWord(5, 4, 3), // ELF
-                            LedWord(6, 8, 5) // ZWÖLF
-                        };
+const LedWord HOURS[] = {
+    LedWord(0, 5, 1), // EINS
+    LedWord(0, 5, 1), // ZWEI
+    LedWord(0, 5, 1), // DREI
+    LedWord(0, 5, 1), // VIER
+    LedWord(0, 5, 1), // FÜNF
+    LedWord(0, 5, 1), // SECHS
+    LedWord(0, 5, 1), // SIEBEN
+    LedWord(0, 5, 1), // ACHT
+    LedWord(0, 5, 1), // NEUN
+    LedWord(0, 5, 1), // ZEHN
+    LedWord(0, 5, 1), // ELF
+    LedWord(0, 5, 1)  // ZWÖLF
+};
 
-const LedWord WORD_FUNK = LedWord(3, 3, 4);
+const LedWord WORD_FUNK = LedWord(5, 0, 1);
 
 #endif /* LedMapping_H */
